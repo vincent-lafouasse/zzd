d6fd44d995ab56cfd1d1048e2f0e3dc93b873d6c
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-10-30 19:14:17 +0100

    read file N chars at a time

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,10 @@ const zzd = @import("zzd");
 const buffer_size: usize = 1024;
 const line_size: usize = 16;
 
+fn processLine(line: []const u8) void {
+    std.debug.print("{s}\n", .{line});
+}
+
 pub fn main() !void {
     const path: []const u8 = ".gitignore";
     var file: std.fs.File = std.fs.cwd().openFile(path, .{}) catch |err| {
@@ -14,10 +18,31 @@ pub fn main() !void {
 
     var buffer: [buffer_size]u8 = undefined;
     var reader: std.fs.File.Reader = std.fs.File.reader(file, &buffer);
-    std.debug.print("reader: {any}\n", .{reader});
 
     const ioReader: *std.Io.Reader = &reader.interface;
-    std.debug.print("interface: {any}\n", .{ioReader});
+
+    while (true) {
+        const maybeLine = ioReader.take(line_size);
+        if (maybeLine) |line| {
+            processLine(line);
+        } else |err| {
+            switch (err) {
+                error.ReadFailed => {
+                    std.debug.print("Failed to read line from file", .{});
+                    return;
+                },
+                error.EndOfStream => {
+                    const len = ioReader.bufferedLen();
+                    if (len == 0) {
+                        return;
+                    } else {
+                        const partialLine = try ioReader.take(ioReader.bufferedLen());
+                        processLine(partialLine);
+                    }
+                },
+            }
+        }
+    }
 
     std.debug.print("ok\n", .{});
 }
