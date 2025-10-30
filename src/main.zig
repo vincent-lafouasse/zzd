914a5dea45fbcf55e5009a57340fa32aa2dbe90f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-10-30 20:27:42 +0100

    refactor

--- src/main.zig
+++ src/main.zig
@@ -8,6 +8,25 @@ fn processLine(line: []const u8) void {
     std.debug.print("{s}\n", .{line});
 }
 
+fn readN(reader: *std.Io.Reader, n: usize) ![]const u8 {
+    if (reader.take(n)) |line| {
+        return line;
+    } else |err| {
+        _ = switch (err) {
+            error.ReadFailed => return err,
+            error.EndOfStream => {
+                const len = reader.bufferedLen();
+                if (len == 0) {
+                    return err;
+                }
+
+                const partialLine = try reader.take(reader.bufferedLen());
+                return partialLine;
+            },
+        };
+    }
+}
+
 pub fn main() !void {
     const path: []const u8 = ".gitignore";
     var file: std.fs.File = std.fs.cwd().openFile(path, .{}) catch |err| {
@@ -22,27 +41,16 @@ pub fn main() !void {
     const ioReader: *std.Io.Reader = &reader.interface;
 
     while (true) {
-        const maybeLine = ioReader.take(line_size);
+        const maybeLine = readN(ioReader, line_size);
         if (maybeLine) |line| {
             processLine(line);
         } else |err| {
-            switch (err) {
-                error.ReadFailed => {
-                    std.debug.print("Failed to read line from file", .{});
-                    return;
-                },
-                error.EndOfStream => {
-                    const len = ioReader.bufferedLen();
-                    if (len == 0) {
-                        return;
-                    } else {
-                        const partialLine = try ioReader.take(ioReader.bufferedLen());
-                        processLine(partialLine);
-                    }
-                },
+            if (err == std.Io.Reader.Error.EndOfStream) {
+                std.debug.print("ok\n", .{});
+            } else {
+                std.debug.print("Read failed\n\t{any}", .{err});
             }
+            break;
         }
     }
-
-    std.debug.print("ok\n", .{});
 }
