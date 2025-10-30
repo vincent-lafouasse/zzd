eab325a64d989845a6c70f31673506d07f701320
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-10-30 22:49:36 +0100

    refactor

--- src/main.zig
+++ src/main.zig
@@ -4,17 +4,6 @@ const zzd = @import("zzd");
 const buffer_size: usize = 1024;
 const line_size: usize = 16;
 
-fn processLine(line: []const u8) void {
-    std.debug.print("{s}\n", .{line});
-}
-
-fn readN(reader: *std.Io.Reader, n: usize) ![]const u8 {
-    return reader.take(n) catch |err| switch (err) {
-        error.ReadFailed => err,
-        error.EndOfStream => if (reader.bufferedLen() == 0) err else try reader.take(reader.bufferedLen()),
-    };
-}
-
 pub fn main() !void {
     const path: []const u8 = ".gitignore";
     var file: std.fs.File = std.fs.cwd().openFile(path, .{}) catch |err| {
@@ -23,13 +12,19 @@ pub fn main() !void {
     };
     defer file.close();
 
-    var buffer: [buffer_size]u8 = undefined;
-    var reader: std.fs.File.Reader = std.fs.File.reader(file, &buffer);
+    var inputBuffer: [buffer_size]u8 = undefined;
+    var reader: std.fs.File.Reader = std.fs.File.reader(file, &inputBuffer);
+
+    var outputBuffer: [buffer_size]u8 = undefined;
+    const stdout: std.fs.File = std.fs.File.stdout();
+    var writer: std.fs.File.Writer = std.fs.File.writer(stdout, &outputBuffer);
 
     const ioReader: *std.Io.Reader = &reader.interface;
+    const ioWriter: *std.Io.Writer = &writer.interface;
 
     while (true) {
-        const line = readN(ioReader, line_size) catch |err| switch (err) {
+        const offset = ioReader.seek;
+        const line = zzd.readN(ioReader, line_size) catch |err| switch (err) {
             std.Io.Reader.Error.ReadFailed => {
                 std.debug.print("Read failed\n\t{any}", .{err});
                 break;
@@ -40,6 +35,9 @@ pub fn main() !void {
             },
         };
 
-        processLine(line);
+        zzd.processLine(line, offset, ioWriter) catch |err| {
+            std.debug.print("Error:\n\t{any}\n", .{err});
+            break;
+        };
     }
 }
--- src/root.zig
+++ src/root.zig
@@ -1,23 +1,15 @@
-//! By convention, root.zig is the root source file when making a library.
 const std = @import("std");
 
-pub fn bufferedPrint() !void {
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
-    const stdout = &stdout_writer.interface;
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try stdout.flush(); // Don't forget to flush!
+pub fn processLine(line: []const u8, offset: usize, writer: *std.Io.Writer) !void {
+    try writer.print("{x}:\t", .{offset});
+    try writer.print("{s}", .{line});
+    try writer.print("\n", .{});
+    try writer.flush();
 }
 
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
+pub fn readN(reader: *std.Io.Reader, n: usize) ![]const u8 {
+    return reader.take(n) catch |err| switch (err) {
+        error.ReadFailed => err,
+        error.EndOfStream => if (reader.bufferedLen() == 0) err else try reader.take(reader.bufferedLen()),
+    };
 }
