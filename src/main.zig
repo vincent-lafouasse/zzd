15ce69fb2ec25cf9dc3498cad3ac0b224b6682c1
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-10-30 18:54:56 +0100

    refactor

--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,23 @@
 const std = @import("std");
 const zzd = @import("zzd");
 
+const buffer_size: usize = 1024;
+const line_size: usize = 16;
+
 pub fn main() !void {
-    const path = ".gitignore";
-    var file = std.fs.cwd().openFile(path, .{}) catch |err| {
+    const path: []const u8 = ".gitignore";
+    var file: std.fs.File = std.fs.cwd().openFile(path, .{}) catch |err| {
         std.debug.print("Failed to open file {s}:\n\t{any}\n", .{ path, err });
         return;
     };
     defer file.close();
 
-    var buffer: [1024]u8 = undefined;
-    const reader = std.fs.File.reader(file, &buffer);
+    var buffer: [buffer_size]u8 = undefined;
+    var reader: std.fs.File.Reader = std.fs.File.reader(file, &buffer);
     std.debug.print("reader: {any}\n", .{reader});
+
+    const input: *std.Io.Reader = &reader.interface;
+    std.debug.print("interface: {any}\n", .{input});
 
     std.debug.print("ok\n", .{});
 }
