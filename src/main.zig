7540235cfcb2a18bc4d2bce394c2c64457f8d07d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-10-31 02:17:21 +0100

    refactor io opening/closing into IoContext

--- src/main.zig
+++ src/main.zig
@@ -5,26 +5,49 @@ const buffer_size: usize = 1024;
 
 const cfg = zzd.Config.default();
 
+const IoContext = struct {
+    infile: ?std.fs.File,
+    outfile: ?std.fs.File,
+    inputBuffer: [buffer_size]u8,
+    outputBuffer: [buffer_size]u8,
+    rawReader: std.fs.File.Reader,
+    rawWriter: std.fs.File.Writer,
+
+    const Self = @This();
+
+    fn open(infilePath: ?[]const u8, outfilePath: ?[]const u8) !Self {
+        var out: Self = undefined;
+
+        const infile = if (infilePath) |path| try std.fs.cwd().openFile(path, .{}) else std.fs.File.stdin();
+        const outfile = if (outfilePath) |path| try std.fs.cwd().openFile(path, .{}) else std.fs.File.stdout();
+
+        out.rawReader = infile.reader(&out.inputBuffer);
+        out.rawWriter = outfile.writer(&out.outputBuffer);
+        out.infile = if (infilePath) |_| infile else null;
+        out.outfile = if (outfilePath) |_| outfile else null;
+
+        return out;
+    }
+
+    fn close(self: Self) void {
+        if (self.infile) |infile| {
+            infile.close();
+        }
+        if (self.outfile) |outfile| {
+            outfile.close();
+        }
+    }
+};
+
 pub fn main() !void {
     const argv = std.os.argv;
 
-    const path: []const u8 = if (argv.len == 1) ".gitignore" else std.mem.span(argv[1]);
+    const infilePath: []const u8 = if (argv.len == 1) ".gitignore" else std.mem.span(argv[1]);
+    var ioCtx = try IoContext.open(infilePath, null);
+    defer ioCtx.close();
 
-    var file: std.fs.File = std.fs.cwd().openFile(path, .{}) catch |err| {
-        std.debug.print("Failed to open file {s}:\n\t{any}\n", .{ path, err });
-        std.process.exit(1);
-    };
-    defer file.close();
-
-    var inputBuffer: [buffer_size]u8 = undefined;
-    var reader: std.fs.File.Reader = std.fs.File.reader(file, &inputBuffer);
-
-    var outputBuffer: [buffer_size]u8 = undefined;
-    const stdout: std.fs.File = std.fs.File.stdout();
-    var writer: std.fs.File.Writer = std.fs.File.writer(stdout, &outputBuffer);
-
-    const ioReader: *std.Io.Reader = &reader.interface;
-    const ioWriter: *std.Io.Writer = &writer.interface;
+    const ioReader: *std.Io.Reader = &ioCtx.rawReader.interface;
+    const ioWriter: *std.Io.Writer = &ioCtx.rawWriter.interface;
 
     while (true) {
         const offset = ioReader.seek;
