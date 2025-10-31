056d0db6681b214513823205829c5a25903bbf94
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-10-31 02:49:28 +0100

    sketch

--- src/config.zig
+++ src/config.zig
@@ -1,3 +1,23 @@
+const Flag = struct {
+    short: []const u8,
+    long: []const u8,
+    argumentName: ?[]const u8,
+    description: []const u8,
+
+    fn columns() Flag {
+        return Flag{
+            .short = "-c",
+            .long = "--columns",
+            .argumentName = "cols",
+            .description = "format <cols> octets per line. Default 16.",
+        };
+    }
+
+    fn help() Flag {
+        return Flag{ .short = "-h", .long = "--help", .argumentName = null, .description = "print this summary." };
+    }
+};
+
 pub const Config = struct {
     line_width: usize,
     colors: bool,
